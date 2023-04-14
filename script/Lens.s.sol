779d3dd1ad4cafc55e73f6e9ca95b789f01e078f
MatteoMer <matteo.merv@gmail.com>
2023-04-15 01:51:28 +0900

    wip again

--- script/Lens.s.sol
+++ script/Lens.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/lens-contracts/core/Lens.sol";
 
-contract CounterScript is Script {
+contract LensScript is Script {
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");

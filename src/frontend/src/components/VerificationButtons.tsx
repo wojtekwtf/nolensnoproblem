a39eaa565b620e5a5cd2d0e43c95986b281d5d50
wojtekwtf <wojciechkulikowski@icloud.com>
2023-04-16 05:08:20 +0900

    Disable verification

--- src/frontend/src/components/VerificationButtons.tsx
+++ src/frontend/src/components/VerificationButtons.tsx
@@ -1,11 +1,11 @@
-import CustomSismoConnectButton from "./SismoConnectButton";
+import FakeSismoButton from "./FakeSismoButton";
 import WorldCoinButton from "./WorldCoinButton";
 
 export default function VerificationButtons() {
 
   return (
-    <div>
-      <CustomSismoConnectButton />
+    <div className="flex flex-col">
+      <FakeSismoButton />
       <p className="text-sm my-1 text-gray-600">OR</p>
       <WorldCoinButton />
     </div>
